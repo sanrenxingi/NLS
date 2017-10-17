955a8cb2116debb76f608aa6bc1bb078d35754de
nyb <nyb@qq.com>
2017-10-17 16:22:51 +0800

    聂雁宾1

--- app/src/main/java/com/example/dell/quarter/view/activity/MainActivity.java
+++ app/src/main/java/com/example/dell/quarter/view/activity/MainActivity.java
@@ -1,42 +1,47 @@
 package com.example.dell.quarter.view.activity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.dell.quarter.R;
+import com.example.dell.quarter.view.fragment.CrossTalkFragment;
+import com.example.dell.quarter.view.fragment.RecommendFragment;
+import com.example.dell.quarter.view.fragment.VideoFragment;
+import com.hjm.bottomtabbar.BottomTabBar;
 
-public class MainActivity extends BaseActivity {
+/**
+ * 作者  聂雁宾
+ */
+public class MainActivity extends AppCompatActivity {
 
-    private TextView zhuce;
+    private BottomTabBar  btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        zhuce.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    @Override
-    int setViewId() {
-        return R.layout.activity_main;
-    }
-
-    //new数据
-    @Override
-    void initData() {
+        setContentView(R.layout.activity_main2);
+        initview();
+        initData();
+        initbtn();
 
     }
-     //找ID
-    @Override
-    void initView() {
-        zhuce = findViewById(R.id.zhuce);
+
+    private void initData() {
+
+    }
+
+    private void initview() {
+        btn =(BottomTabBar) findViewById(R.id.bottom_tab_bar);
+    }
+
+    private void initbtn() {
+
+        btn.init(getSupportFragmentManager())
+                .addTabItem("推荐", R.mipmap.ic_launcher, RecommendFragment.class)
+                .addTabItem("段子", R.mipmap.ic_launcher, CrossTalkFragment.class)
+                .addTabItem("视频", R.mipmap.ic_launcher, VideoFragment.class);
+
     }
 }
