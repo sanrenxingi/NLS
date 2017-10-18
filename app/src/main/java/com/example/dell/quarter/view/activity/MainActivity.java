f6012fb8637281b50124d1f0a7bc9ca7fbe76a0c
shizhaoyu <shizhaoyu@126.com>
2017-10-18 18:29:07 +0800

    qqq

--- app/src/main/java/com/example/dell/quarter/view/activity/MainActivity.java
+++ app/src/main/java/com/example/dell/quarter/view/activity/MainActivity.java
@@ -3,10 +3,8 @@ package com.example.dell.quarter.view.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
-
 import com.example.dell.quarter.R;
 import com.example.dell.quarter.view.fragment.CrossTalkFragment;
 import com.example.dell.quarter.view.fragment.RecommendFragment;
@@ -19,8 +17,9 @@ import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
  */
 public class MainActivity extends AppCompatActivity {
     private ImageView touxiangimg;
-    private BottomTabBar bottomTabBar;
+
     private SlidingMenu menu;
+    private BottomTabBar bottomTabBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +33,11 @@ public class MainActivity extends AppCompatActivity {
         touxiangimg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-          menu.showMenu();
+                menu.showMenu();
             }
         });
     }
+
     private void initMenu() {
 //设置从哪里滑出
         menu.setMode(SlidingMenu.LEFT);
@@ -47,27 +47,27 @@ public class MainActivity extends AppCompatActivity {
         menu.setBehindOffsetRes(R.dimen.du);
         //设置渐入渐出的效果
         menu.setFadeDegree(0.35f);
-        menu.attachToActivity(this,SlidingMenu.SLIDING_CONTENT);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
         menu.setMenu(R.layout.slidingmenu_layout);
 
     }
 
     private void initData() {
         menu = new SlidingMenu(this);
+        initbtn();
     }
+
     private void initview() {
         bottomTabBar = (BottomTabBar) findViewById(R.id.bottom_tab_bar);
         touxiangimg = (ImageView) findViewById(R.id.touxiangimg);
     }
 
     private void initbtn() {
+
         bottomTabBar.init(getSupportFragmentManager())
-                .addTabItem("推荐", R.mipmap.ic_launcher, RecommendFragment.class)
-                .addTabItem("段子", R.mipmap.ic_launcher, CrossTalkFragment.class)
-                .addTabItem("视频", R.mipmap.ic_launcher, VideoFragment.class)
-
-        ;
-
+                .addTabItem("推荐", R.mipmap.tuijian_select, R.mipmap.tuijian_default, RecommendFragment.class)
+                .addTabItem("段子", R.mipmap.duanzi_select, R.mipmap.duanzi_default, CrossTalkFragment.class)
+                .addTabItem("视频", R.mipmap.video_select, R.mipmap.video_defaults, VideoFragment.class);
 
     }
 }
