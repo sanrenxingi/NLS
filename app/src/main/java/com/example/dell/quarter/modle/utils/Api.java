8f12cabaed7294059dac3ac8f908dd2939004567
shizhaoyu <shizhaoyu@126.com>
2017-10-23 13:07:26 +0800

    段子

--- app/src/main/java/com/example/dell/quarter/modle/utils/Api.java
+++ app/src/main/java/com/example/dell/quarter/modle/utils/Api.java
@@ -7,6 +7,6 @@ package com.example.dell.quarter.modle.utils;
 public class Api {
     public static String REGISTER="http://172.17.29.27/quarter/user/addUser?";
     public static String Login="http://172.17.29.27/quarter/user/addLogin?";
-
+    public static String Cross_Talk_URL="http://172.17.29.27/quarter/character/select_character";
 
 }
